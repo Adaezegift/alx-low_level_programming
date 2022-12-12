2e45e98af8f74fb281746b96791003943d0ddd42
Adaezegift <adaezegift@yahoo.com>
2022-12-11 16:58:11 -0800

    alphabeth

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,8 @@ int main(void)
 	for (letter - 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	for (letter ='A'; letter <='z'; letter ++)
+	for (letter = 'A'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
-	
 	return (0);
