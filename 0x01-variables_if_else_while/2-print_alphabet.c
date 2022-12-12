e228cfd3b0ba2eecf30ccd6092ad59901a5973c5
Adaezegift <adaezegift@yahoo.com>
2022-12-11 16:40:47 -0800

    betty

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabeth in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return(0);
+}
