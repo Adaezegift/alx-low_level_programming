1743b48e8e6099e4ecc65d393921f3a232d203fa
Adaezegift <adaezegift@yahoo.com>
2022-12-11 16:28:36 -0800

    annoying git commit -m annoying git push vi 1-last_digit.c git commit -m annoying

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 	{
 		printf("and is greater than 5");
 	}
-	if (x==0)
+	if (x == 0)
 	{
 		printf("and is 0");
 	}
-	if (x < 6 && x !=0)
+	if (x < 6 && x != 0)
 	{
 		printf("and is less than 6 and not 0");
 	}
