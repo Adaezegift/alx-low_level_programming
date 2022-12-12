d0c59faf480a7e56e1693018cf29eb47d6968dab
Adaezegift <adaezegift@yahoo.com>
2022-12-11 18:53:09 -0800

    takky

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num 0; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
