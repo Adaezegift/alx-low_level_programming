ab8bbf528da83c8f4885f8c19c51b1631a1b99cf
Adaezegift <adaezegift@yahoo.com>
2022-12-12 16:55:37 -0800

    peace

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful"-Dora Korpar, 2-15-10-19",\n", to standard error;
+ * main - Prints "and that piece of art is useful"-Dora Korpar, 2-15-10-19",
+ * followed by a new line to standard error;
  * Return: (1)
- * 
-
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 	return (1);
 }
