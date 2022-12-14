905ea9b3c964b1252b37939f1cb60aced6053b17
Adaezegift <adaezegift@yahoo.com>
2022-12-14 03:16:03 -0800

    peace

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10(void) - print 10 times the alphabet, in lower case
+ * print_alphabet_x10 - print 10 times the alphabet, in lower case
 */
 
 void print_alphabet_x10(void)
