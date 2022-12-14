865630b873bb07cc16c19cf95e3d005d3f76ddbf
Adaezegift <adaezegift@yahoo.com>
2022-12-14 03:10:05 -0800

    joy

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,5 +13,5 @@ void print_alphabet_x10(void)
 	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	_putchar('\n');
-	}
+	_putchar('/n');
+} 
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _islower - check if a char is lowercase
+ * _islower - checks if a character is lowercase.
  * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
 int _islower(int c)
 {
-	if (c >= 'a' && <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
