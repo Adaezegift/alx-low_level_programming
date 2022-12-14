bcb530035593d066a365a5a9e6a28cfb51d63504
Adaezegift <adaezegift@yahoo.com>
2022-12-14 02:12:35 -0800

    nawa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10(void) -function that prints 10 times the alphabet,
+ * print_alphabet_x10(void) - function that prints 10 times the alphabet,
 */
 
 void print_alphabet_x10(void)
