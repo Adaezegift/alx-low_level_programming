1cacaa7fb2daae596c4d78ffcf5dab6c5540c18d
Adaezegift <adaezegift@yahoo.com>
2022-12-20 16:17:25 -0800

    sup

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string
+ * Return: lemgjt
+ */
+int _strlen(char *s)
+{
+	int longi = 0
+
+		while (*s != '\0')
+		{
+			longi++;
+			s++;
+		}
+	return (longi);
+}
