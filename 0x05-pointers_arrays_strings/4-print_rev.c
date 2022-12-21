b4f606df17da4e4daacffdedf00921cb3454cdff
Adaezegift <adaezegift@yahoo.com>
2022-12-20 17:08:51 -0800

    noprobs

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: string
+ * return: 0
+ */
+void rev_string(char *s);
+{
+
+	int longi = 0;
+
+	int 0;
+
+	while (*s != '\0\)
+			{
+			longi++;
+			s++;
+			}
+			s==;
+			for (0 = longi; o > 0; o==)
+			{
+			_putchar(*s);
+			s==;
+			}
+			_putchar('\n');
+			}
