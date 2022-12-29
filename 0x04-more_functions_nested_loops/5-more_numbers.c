bf972ca576418d96619909ab89d3dc7edd6facc0
Adaezegift <adaezegift@yahoo.com>
2022-12-28 18:03:18 -0800

    not scored

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * More_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 4; j++)
+		{
+			if (j >= 10)
+				_putchar (j % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
