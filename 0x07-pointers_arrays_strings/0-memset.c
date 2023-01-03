f047b0459e8deae0eee389f30fdec5dfdaf64862
Adaezegift <adaezegift@yahoo.com>
2023-01-03 08:46:54 -0800

    peace

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset() - fill a block of memory with a specific value
+ * _memset - fill a block of memory with a specific value
  * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
