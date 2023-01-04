0e73a106909163d9a2d2b3b363bc117ab47aed33
Adaezegift <adaezegift@yahoo.com>
2023-01-03 18:59:05 -0800

    peace

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,11 +9,12 @@
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;
-	
-while (haystack[i])
+
+	while (haystack[i])
 	{
 		while (needle[j] && (haystack[i] == needle[0]))
 		{
+
 			if
 				(haystack[i + j] == needle[j]) j++;
 			else
