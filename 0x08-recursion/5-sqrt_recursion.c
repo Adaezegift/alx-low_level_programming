14747c038ce3d5476a2539a2cd1b8eb0221aa9dc
Adaezegift <adaezegift@yahoo.com>
2023-01-05 06:32:15 -0800

    finally

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,11 +6,11 @@
  *
  * Return: int
  */
-int check (int a, int b)
+int check(int a, int b)
 {
-	if (a *a == b)
+	if (a * a == b)
 		return (a);
-	if (a *a > b)
+	if (a * a > b)
 		return (-1);
 	return (check(a + 1, b));
 }
