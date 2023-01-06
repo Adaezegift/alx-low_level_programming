012aaf1b7f2b8f76984fe8fc1115cfd72d3225b1
Adaezegift <adaezegift@yahoo.com>
2023-01-05 20:05:44 -0800

    my score

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,18 @@
 #include "main.h"
 /**
- * str_checker - check if two strings are identical
- * @s1: string_1 base address
- * @s2: string_2 base address
- * @i: left index
- * @j: special index. (joker)
- * Return: 1 if s is palindrome, 0 otherwise
- */
-int str_checker(char *s1, char *s2, int i, int j)
-{
-	if (s1[i] == '\0' && s2[j] == '\0')
-		return (1);
-	if (s1[i] == s2[j])
-		return
-			(str_checker(s1, s2, i + 1, j + 1));
-       if (s1[i] == '\0' && s2[j]== '*')
-	       return
-		       (str_checker(s1, s2, i + 1, j) ||str_checker(s1, s2, i, j +1));
-	return (0);
-}
-/**
  * wildcmp - check if  strings could be considered identical
  * @s1: base address for string
  * @s2: base address for string
- * Return: 1 if ae considered identical
+ * Return: 1 if ae considered identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (str_checker(s1, s2, 0, 0));
+	if
+		(*s2 == '\0')
+			return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (s2 == ")
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
 }
