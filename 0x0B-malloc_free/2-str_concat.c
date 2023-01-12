95b1c043f299cc26ade06fce62d74f64838e07a2
Adaezegift <adaezegift@yahoo.com>
2023-01-11 19:01:52 -0800

    peace

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,32 +8,34 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-		i = ci = 0;
-		while (s1[i] != '\0')
-		i++;
-		while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-	if (conct == NULL)
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
+	{
+		i = 0
+			while (s1[i++] != '\0')
+				len1++;
+	}
+
+	if (s2 != NULL)
+	{
+		i = 0
+			while (s2[i++] != '\0')
+				len2++;
+	}
+	lem = len1 <= len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
