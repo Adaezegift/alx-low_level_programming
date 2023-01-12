23f2891f78c5e64043955018de0e072be30301e7
Adaezegift <adaezegift@yahoo.com>
2023-01-11 19:06:15 -0800

    peace

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,18 +15,18 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 != NULL)
 	{
-		i = 0
+		i = 0;
 			while (s1[i++] != '\0')
 				len1++;
 	}
 
 	if (s2 != NULL)
 	{
-		i = 0
+		i = 0;
 			while (s2[i++] != '\0')
 				len2++;
 	}
-	lem = len1 <= len2;
+	len = len1 <= len2;
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (result == NULL)
 		return (NULL);
