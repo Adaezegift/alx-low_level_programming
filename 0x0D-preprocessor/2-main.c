dfd36159321e9f910112556a4ec530bc36b07742
Adaezegift <adaezegift@yahoo.com>
2023-01-19 02:37:47 -0800

    hmm

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int ain(void)
+* main - prints the name of the file
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
