0aa9006c36747da51fd9faf926d3c04c8d93f4a7
adaezegift <adaezegift@yahoo.com>
2023-01-31 06:48:36 -0800

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
 * print_listint - prints all the elements of a linked list
 * @h: linked list of type listint_t to print
-* return: number of nodes
+* Return: number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
